53c662febb060b20f2d25f0b39601e12c905b06b
LoanH <lheniart@be-infotech.fr>
2025-01-14 15:17:09 +0100

    feat(project) add navigation and input on the home page

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,44 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <nav>
+    <router-link to="/">Estimation</router-link>
+    <router-link to="/history">Historique</router-link>
+  </nav>
   <RouterView />
 </template>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: DM Sans, Avenir, Helvetica, Arial, sans-serif;
+}
 
+html {
+  background-color: #D9D9D9;
+}
+nav {
+  padding: 0 30px;
+  width: 100%;
+  height: 90px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 50px;
+  background-color: #384454;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+
+  a{
+    color: #D4D3DC;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    &.router-link-exact-active {
+      border-bottom: solid 5px #317AC1;
+      border-radius: 0px 0 5px 5px;
+    }
+  }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,47 @@
 <script setup lang="ts">
+  import {ref} from "vue";
+
+  const annualInvestment = ref<number>(0);
+  const investmentDuration = ref<number>(0);
+  const interestRate = ref<number>(0);
+
 </script>
 
 <template>
   <main>
-    test
+    <header class="row g-3" id="header-input">
+      <div class="col-md-2">
+        <label class="form-label" for="investissementInput">Investissement annuel</label>
+        <div class="input-group">
+          <input id="investissementInput" v-model="annualInvestment" class="form-control" min="0" step="100"
+                 type="number">
+          <span class="input-group-text">€</span>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <label class="form-label" for="durationInput">Durée de l'investissement</label>
+        <div class="input-group">
+          <input id="durationInput" v-model="investmentDuration" class="form-control" max="60" min="0" step="1"
+                 type="number">
+          <span class="input-group-text">Année</span>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <label class="form-label" for="pourcentageInput">Taux</label>
+        <div class="input-group">
+          <input id="pourcentageInput" v-model="interestRate" class="form-control"  step="0,1"
+                 type="number">
+          <span class="input-group-text">%</span>
+        </div>
+      </div>
+    </header>
   </main>
 </template>
+
+<style scoped>
+ #header-input{
+   align-items: center;
+   justify-content: center;
+   height: 120px;
+ }
+</style>
